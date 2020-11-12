1dc568ad8787aee3b54ea58f2491ccf8f33176d4
apish1604 <kapinsangwan@gmail.com>
2020-11-12 15:02:49 +0530

    mogodb atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/ResultPublishing-api',{
+const connection ="mongodb+srv://kapinsangwan:kapinsangwan@resultpublishingapplica.3rlzz.mongodb.net/rpsdatabase?retryWrites=true&w=majority"
+mongoose.connect(connection,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:false,
     useUnifiedTopology:true
 })
+
+//ResultPublishing-api
